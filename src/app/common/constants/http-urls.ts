4382ddfeb61b390937b880af12ab27b0f42e14e4
Suraj Gupta <surajgupta.work@gmail.com>
2021-05-20 17:33:34 +0530

    url changes

--- src/app/common/constants/http-urls.ts
+++ src/app/common/constants/http-urls.ts
@@ -1,5 +1,5 @@
-export const baseURL = 'http://localhost:8080'
-// export const baseURL = 'https://sports-geek.herokuapp.com'
+// export const baseURL = 'http://localhost:8080'
+export const baseURL = 'https://sports-geek.herokuapp.com'
 export const usersBaseURL = baseURL +'/users'
 export const matchBaseURL = baseURL + '/matches'
 export const contestBaseURL = baseURL + '/contest'
